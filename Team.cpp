ee5ddf469cfc8388eb7a70f5f5bab55221a20f39
Evan Hanzelman <ehanz1997@gmail.com>
2023-03-29 10:22:42 -0400

    Retrieve row to get certain player info

--- Team.cpp
+++ Team.cpp
@@ -7,10 +7,28 @@ Team :: Team ()
 
 }
 
+// Gets a vector of a specific player on the team
 std::vector <std::string>
 Team :: getPlayer (std::string playerName)
 {
+    int p_iterator = 0;
+    std::vector <std::string> p_vector;
 
+    for (int i = 0; i < t_matrix.size (); i++)
+    {
+        if (t_matrix[i][1] == playerName)
+        {
+            p_iterator = i;
+            break;
+        }
+    }
+
+    for (int j = 0; j < t_matrix[p_iterator].size (); j++)
+    {
+        p_vector.push_back (t_matrix[p_iterator][j]);
+    }
+
+    return p_vector;
 }
 
 // Gets the vector of the team and their stats
