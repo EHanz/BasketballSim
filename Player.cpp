42ef684119bbcc24b14ac2306b6460babaed609f
Evan Hanzelman <ehanz1997@gmail.com>
2023-03-29 11:58:17 -0400

    Retrieved individual player stats

--- Player.cpp
+++ Player.cpp
@@ -4,3 +4,15 @@ Player :: Player ()
 {
     
 }
+
+std::vector <float>
+Player :: getPlayerStats ()
+{
+    return p_stats;
+}
+
+void
+Player :: setPlayerStats (Team* t_ptr, std::string playerName)
+{
+    p_stats = t_ptr -> getPlayerStats (playerName);
+}
--- Player.hpp
+++ Player.hpp
@@ -1,13 +1,25 @@
 #ifndef PLAYER_HPP
 #define PLAYER_HPP
 
+#include <iostream>
+#include <vector>
+
+#include "Team.hpp"
+
 class Player
 {
     public: 
         Player ();
         ~Player ();
 
+        std::vector <float>
+        getPlayerStats ();
+
+        void
+        setPlayerStats (Team* t_ptr, std::string playerName);
+
     private:
+        std::vector <float> p_stats;
 
 };
 
