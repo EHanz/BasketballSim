650599873616da6ae842e37d7d10db42ed530d6a
Evan Hanzelman <ehanz1997@gmail.com>
2023-03-31 14:50:17 -0400

    Added to header

--- ComparePlayer.hpp
+++ ComparePlayer.hpp
@@ -2,3 +2,21 @@
 // Get the stats of each player
 // Assign the stats to individual vars for each player
 // Compare the stats of each player head to head
+
+#ifndef COMPARE_PLAYER_HPP
+#define COMPARE_PLAYER_HPP
+
+#include <iostream>
+#include <vector>
+
+class ComparePlayer
+{
+    public:
+
+    
+    private:
+        std::vector <float> player_one_stats;
+        std::vector <float> player_two_stats;
+};
+
+#endif
