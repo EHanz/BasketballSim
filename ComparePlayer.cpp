9f227d202f16ce3244895bf5fe9d4695b4d7af06
Evan Hanzelman <ehanz1997@gmail.com>
2023-04-03 21:49:37 -0400

    Updated Functions

--- ComparePlayer.cpp
+++ ComparePlayer.cpp
@@ -5,3 +5,44 @@
 */
 
 #include "ComparePlayer.hpp"
+#include "Player.hpp"
+
+// Standard Constructor
+ComparePlayer :: ComparePlayer ()
+{
+
+}
+
+// Sets the stats of Player One only
+void
+ComparePlayer :: setPlayerOneStats (Player* p1)
+{
+
+}
+
+// Sets the stats of Player Two only
+void
+ComparePlayer :: setPlayerTwoStats (Player* p2)
+{
+
+}
+
+// Sets the stats of both players at the same time
+// TODO: Look into if I should use globals still or switch to locals
+// TODO: Look into whether to use pointers or references for p1 & p2
+// TODO: Get player's names assigned in Player Class
+void
+ComparePlayer :: setPlayerStats (Player* p1, Player* p2)
+{
+    player_one_stats = p1 -> getPlayerStats ();
+    player_two_stats = p2 -> getPlayerStats (); 
+
+    if (p1->assists > p2->assists)
+    {
+        std::cout << "Player One Has More Assists Per Game" << "\n";
+    }
+    else 
+    {
+        std::cout << "Player Two Has More Assists Per Game" << "\n";
+    }
+}
