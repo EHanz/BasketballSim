168ea3ee66d8baf69a34b4a5145ed98aeeca5d3c
Evan Hanzelman <ehanz1997@gmail.com>
2023-04-10 16:48:44 -0400

    Can read in csv

--- Team.cpp
+++ Team.cpp
@@ -11,14 +11,29 @@ Team :: Team ()
     
 }
 
-std::vector <std::string>
+std::vector <std::vector <std::string>>
 getTeamStats ()
 {
-
+    
 }
 
 void
-setTeamStats ()
+setTeamStats (const std::string& filename)
 {
+    std::ifstream data (filename);
+    std::string line;
+
+    std::cout << "Loading League Matrix..." << std::endl;
+
+    while (std::getline (data,line))
+    {
+        std::stringstream lineStream (line);
+        std::string cell;
+        std::vector <std::string> parsedRow;
+        while (std::getline (lineStream,cell,','))
+        {
+            parsedRow.push_back (cell);
+        }
+    }
     
 }
