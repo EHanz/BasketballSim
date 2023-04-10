8c8a037944d82f84e0183f45e3e0907743876cf7
Evan Hanzelman <ehanz1997@gmail.com>
2023-04-10 19:01:03 -0400

    Added Function to load Team

--- Team.cpp
+++ Team.cpp
@@ -10,3 +10,31 @@ Team :: Team ()
 {
     
 }
+
+std::vector<std::vector<std::string>>
+Team :: getTeamMatrix ()
+{
+    return team_matrix;
+}
+
+void
+Team :: setTeamMatrix (const std::string &filename)
+{
+    std::ifstream data (filename);
+    std::string line;
+
+    std::cout << "Loading Team Matrix..." << std::endl;
+
+    while (std::getline (data,line))
+    {
+        std::stringstream lineStream (line);
+        std::string cell;
+        std::vector <std::string> parsedRow;
+        while(std::getline (lineStream,cell,','))
+        {
+            parsedRow.push_back (cell);
+        }
+
+        team_matrix.push_back (parsedRow);
+    }
+}
