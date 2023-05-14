3cff75b60492634d0df17a386a59421d5cd51cb1
Evan Hanzelman <ehanz1997@gmail.com>
2023-05-14 18:51:14 -0400

    Update ComparePlayer.cpp

--- ComparePlayer.cpp
+++ ComparePlayer.cpp
@@ -34,7 +34,6 @@ ComparePlayer :: assignVecToFloats ()
 }
 
 // Below are the series of stat comparison functions
-// Might try and find a way to do all of this within one function
 int
 ComparePlayer :: comparePlayerAssists ()
 {
